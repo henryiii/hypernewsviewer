e3624bc585fe4b835285d8e8d2e2416df50d23f4
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2021-02-02 00:40:05 -0500

    feat: add tree view

--- src/hypernewsviewer/model/structure.py
+++ src/hypernewsviewer/model/structure.py
@@ -3,14 +3,16 @@
 from __future__ import annotations
 
 from pathlib import Path
+from typing import Iterator
 
 import click
 from rich import print
 from rich.markdown import Markdown
 from rich.panel import Panel
 from rich.table import Table
+from rich.tree import Tree
 
-from .parser import URCMessage
+from .parser import URCBase, URCMain, URCMessage
 
 DIR = Path(__file__).parent.resolve()
 
@@ -45,17 +47,60 @@ def list_fn(ctx: click.Context) -> None:
             )
         )
 
-    msg_files = [path for path in rootpath.glob("*.html,urc")]
-    urc_msgs = [URCMessage.from_path(f) for f in msg_files]
     t = Table(title="Messages")
     t.add_column("#", style="cyan")
     t.add_column("N", style="green")
     t.add_column("Title")
-    for m in sorted(urc_msgs, key=lambda x: x.num):
-        msgs = root.joinpath(m.responses.lstrip("/")).glob("*.html,urc")
+
+    for m in get_msgs(rootpath):
+        msgs = get_msg_paths(root / m.responses.lstrip("/"))
         entries = len(list(msgs))
         t.add_row(str(m.num), str(entries), m.title)
+
     print(t)
+
+
+@main.command()
+@click.pass_context
+def tree(ctx: click.Context) -> None:
+    root: Path = ctx.obj["root"]
+    rootpath: Path = ctx.obj["rootpath"]
+
+    msg: URCBase
+    if rootpath.stem.isdigit():
+        msg = URCMessage.from_path(rootpath.with_suffix(".html,urc"))
+    else:
+        msg = URCMain.from_path(rootpath.with_suffix(".html,urc"))
+    tree = Tree(
+        ":open_file_folder: "
+        f"[link file://{rootpath}]{rootpath.relative_to(root)}: {msg.title}"
+    )
+    walk_tree(rootpath, tree)
+    print(tree)
+
+
+def get_msg_paths(directory: Path) -> list[Path]:
+    return sorted(directory.glob("*.html,urc"), key=lambda x: int(x.stem))
+
+
+def get_msgs(directory: Path) -> Iterator[URCMessage]:
+    for path in get_msg_paths(directory):
+        yield URCMessage.from_path(path)
+
+
+def walk_tree(directory: Path, tree: Tree) -> None:
+    for path in get_msg_paths(directory):
+        folder = path.with_suffix("")
+        emoji = ":open_file_folder:" if folder.exists() else ":file_folder:"
+        try:
+            msg_title = URCMessage.from_path(path).title
+        except UnicodeDecodeError:
+            ":cross_mark:"
+        branch = tree.add(
+            f"{emoji} [link file://{folder}]{path.stem}: {msg_title}",
+        )
+        if folder.exists():
+            walk_tree(folder, branch)
 
 
 if __name__ == "__main__":
