082d8a593e9e33129510b9f2517f8c64331453f8
Henry Schreiner <henryschreineriii@gmail.com>
2021-09-02 11:52:59 -0400

    fix: config points to correct place

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ threads = int(os.environ.get("GUNICORN_THREADS", "1"))
 
 forwarded_allow_ips = "*"
 secure_scheme_headers = {"X-Forwarded-Proto": "https"}
+
+wsgi_app = "hypernewsviewer.app:application"
