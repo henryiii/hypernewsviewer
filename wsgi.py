c9a8f59c4d99ada8a14d47b02c5334cb605b2dd1
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2023-09-18 11:08:51 -0400

    chore: cleaner logs (#27)
    * chore: cleaner logs
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    * 'Refactored by Sourcery' (#28)
    
    Co-authored-by: Sourcery AI <>
    
    ---------
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,20 @@
 from __future__ import annotations
 
+import logging
+
 from hypernewsviewer.app import app as application
+
+
+class HealthCheckFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord) -> bool:
+        msg = record.getMessage()
+        return "kube-probe" not in msg or '"GET / HTTP/1.1" 200' not in msg
+
+
+# Remove health check from application server logs
+logging.getLogger("uvicorn.access").addFilter(HealthCheckFilter())
+
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     application.run()
